54bcead433b479ca43a934bb1f2f22307f6d1c30
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-11-21 20:26:21 -0300

    first commit

---
+++ src/main/java/com/crowdar/examples/steps/LoginSteps.java
@@ -0,0 +1,30 @@
+package com.crowdar.examples.steps;
+
+import com.crowdar.core.PageSteps;
+import com.crowdar.core.actions.MobileActionManager;
+import com.crowdar.examples.constants.LoginConstants;
+import com.crowdar.examples.services.HomeService;
+import com.crowdar.examples.services.LoginService;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * This class handles the steps in the features files and connects with the service in case of having business logic.
+ * Otherwise, if it is a simple action, like clicking a button and it has nothing related to business logic, is correct to put here.
+ */
+public class LoginSteps extends PageSteps {
+
+    @Given("The app is loaded correctly")
+    @Then("Login page is displayed")
+    public void isLoginPageVisible() {
+        LoginService.isViewLoaded();
+    }
+
+
+    @When("The user logs in the application with: (.*), (.*)")
+    public void doLoginProcess(String email, String password) {
+        LoginService.doLogin(email, password);
+    }
+
+}
